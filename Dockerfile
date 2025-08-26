b3a17c7621c4a57f2fabd0adf1240691815e19c1
Tupap1 <andresdavidcantillo@gmail.com>
2025-08-25 19:56:31 -0500

    parcial setup hot reloading

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,29 @@
-FROM golang:1.25
+FROM golang:1.24.5
 
 # Set the working directory inside the container
 WORKDIR /app
 
 # Copy the Go modules manifests
-COPY go.mod go.sum ./
+COPY go.* ./
 
 # Download Go modules
 RUN go mod download
 
-# Copy the source code into the container
 COPY . .
+RUN go install github.com/air-verse/air@latest
+RUN air init
+#RUN air -c .air.toml
+
+
+# Copy the source code into the container
 
 # Build the Go application
-RUN go build -o main .
+RUN go build -o main main.go
 
 # Expose the application port
 EXPOSE 8080
 
 # Command to run the executable
-CMD ["./main"]
+CMD ["air", "-c", ".air.toml"]
+
+
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
+	//"log"
 	"github.com/gofiber/fiber/v2"
-	"github.com/tupap1/gologin/server/repository"
+	//"github.com/tupap1/gologin/server/repository"
 
 )
 
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println("APP IS RUNNING...")
 
 	// 1. Inicializa la base de datos.
-	repository.InitDatabase()
+	//repository.InitDatabase()
 
 
 	// 2. Crea la instancia de Fiber.
@@ -23,7 +23,13 @@ func main() {
 	// 3. Pasa 'app' y la conexión 'db' a la función que configura las rutas.
 	// Esta acción utiliza la variable 'db', resolviendo el error del compilador.
 	// 4. Inicia el servidor.
-	log.Fatal(app.Listen(":3000"))
+	//log.Fatal(app.Listen(":8080"))
+
+	app.Get("/hola", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!sss")
+	})
+
+	app.Listen(":8080")
 }
 
 
